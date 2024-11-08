1ef534887914cbaeb31187342170edea2a35051b
zedpaixd <planta636@gmail.com>
2024-11-09 00:03:11 +0200

    addition to matching

--- app/server/app.py
+++ app/server/app.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI # type: ignore
 
 from server.routes.applicant import router as ApplicantRouter # type: ignore
 from server.routes.company import router as CompanyRouter
+from server.routes.matching import router as MatchingRouter
 
 app = FastAPI() # type: ignore
 
 app.include_router(ApplicantRouter, tags=["Applicant"], prefix="/applicant")
 app.include_router(CompanyRouter, tags=["Company"], prefix="/company")
+app.include_router(MatchingRouter, tags=["Matching"], prefix="/matching")
 
 
 @app.get("/", tags=["Root"])
