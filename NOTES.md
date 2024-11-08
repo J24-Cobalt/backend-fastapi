e552b7b3dce2ec6b7ec54509dc30844d213f9dd1
zedpaixd <planta636@gmail.com>
2024-11-08 18:44:13 +0200

    backend works

--- NOTES.md
+++ NOTES.md
@@ -10,3 +10,15 @@ mongod --dbpath ~/data/db --fork --logpath ~/data/mongod.log
 
 to stop:
 tail -n 10 ~/data/mongod.log
+
+
+# Server starting
+-- WSL / Linux
+source venv/bin/activate
+
+-- Windows 
+call venv/bin/activate/activate.bat  (unsure needa test)
+
+pip install requirements.txt
+
+python3 app/main.py
--- app/server/routes/applicant.py
+++ app/server/routes/applicant.py
@@ -22,3 +22,44 @@ async def add_applicant_data(applicant: ApplicantSchema = Body(...)):
     applicant = jsonable_encoder(applicant)
     new_applicant = await add_applicant(applicant)
     return ResponseModel(new_applicant, "Applicant added successfully.")
+
+@router.get("/", response_description="Applicants retrieved")
+async def get_applicants():
+    applicants = await retrieve_applicants()
+    if applicants:
+        return ResponseModel(applicants, "Applicant data retrieved successfully")
+    return ResponseModel(applicants, "Empty list returned")
+
+
+@router.get("/{id}", response_description="Applicant data retrieved")
+async def get_applicant_data(id):
+    applicant = await retrieve_applicant(id)
+    if applicant:
+        return ResponseModel(applicant, "Applicant data retrieved successfully")
+    return ErrorResponseModel("An error occurred.", 404, "Applicant doesn't exist.")
+
+@router.put("/{id}")
+async def update_applicant_data(id: str, req: UpdateApplicantModel = Body(...)):
+    req = {k: v for k, v in req.dict().items() if v is not None}
+    updated_applicant = await update_applicant(id, req)
+    if updated_applicant:
+        return ResponseModel(
+            "Applicant with ID: {} name update is successful".format(id),
+            "Applicant name updated successfully",
+        )
+    return ErrorResponseModel(
+        "An error occurred",
+        404,
+        "There was an error updating the applicant data.",
+    )
+
+@router.delete("/{id}", response_description="Applicant data deleted from the database")
+async def delete_applicant_data(id: str):
+    deleted_applicant = await delete_applicant(id)
+    if deleted_applicant:
+        return ResponseModel(
+            "Applicant with ID: {} removed".format(id), "Applicant deleted successfully"
+        )
+    return ErrorResponseModel(
+        "An error occurred", 404, "Applicant with id {0} doesn't exist".format(id)
+    )
