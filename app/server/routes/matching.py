4560cb633d0b279293109f005534e6a5f20d25f2
zedpaixd <planta636@gmail.com>
2024-11-09 02:02:46 +0200

    company to users

--- app/server/routes/matching.py
+++ app/server/routes/matching.py
@@ -12,12 +12,21 @@ from server.models.matching import (
 router = APIRouter()
 
 
-@router.get(
-    "/{email}-to-many", response_description="Match applicant to many companies"
-)
+@router.get("/matching/applicant/{email}", response_description="Match applicant to many companies")
 async def match_applicant_to_companies(email):
     applicant = await retrieve_applicant(email)
     companies = await retrieve_companies()
     if applicant:
+        #magic is done here
         return f"Found: {applicant} and {companies}"
     return ErrorResponseModel("An error occurred.", 404, "Applicant doesn't exist.")
+
+
+@router.get("/matching/company/{email}", response_description="Match company to many applicants")
+async def match_company_to_applicant(email):
+    company = await retrieve_company(email)
+    applicants = await retrieve_applicants()
+    if company:
+        #magic is done here
+        return f"Found: {company} and {applicants}"
+    return ErrorResponseModel("An error occurred.", 404, "Company doesn't exist.")
