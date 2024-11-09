a8d9ce3f5e32776e2b3846872fe5856a8b507436
zedpaixd <planta636@gmail.com>
2024-11-09 02:18:01 +0200

    matching scaffolding for algorithm maybe and sample data

--- app/server/routes/matching.py
+++ app/server/routes/matching.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Body  # type: ignore
 from fastapi.encoders import jsonable_encoder  # type: ignore
 from server.database import retrieve_applicants, retrieve_applicant
 from server.company_database import retrieve_companies, retrieve_company
-
+from server.util.matchmaking import match_fitting_applicant, match_fitting_company
 from server.models.matching import (
     ErrorResponseModel,
     ResponseModel,
@@ -17,8 +17,8 @@ async def match_applicant_to_companies(email):
     applicant = await retrieve_applicant(email)
     companies = await retrieve_companies()
     if applicant:
-        #magic is done here
-        return f"Found: {applicant} and {companies}"
+        matched_companies = match_fitting_company(applicant, companies)
+        return f"{applicant} | {matched_companies}"
     return ErrorResponseModel("An error occurred.", 404, "Applicant doesn't exist.")
 
 
@@ -27,6 +27,6 @@ async def match_company_to_applicant(email):
     company = await retrieve_company(email)
     applicants = await retrieve_applicants()
     if company:
-        #magic is done here
-        return f"Found: {company} and {applicants}"
+        matched_applicants = match_fitting_applicant(company, applicants)
+        return f"{company} | {matched_applicants}"
     return ErrorResponseModel("An error occurred.", 404, "Company doesn't exist.")
--- app/server/util/matchmaking.py
+++ app/server/util/matchmaking.py
@@ -1,2 +1,12 @@
-def fit():
-    pass
+def match_fitting_company(applicant, companies):
+    with open("sample_entry_companies.txt", "w") as file:
+        for company in companies:
+            file.write(str(company) + "\n")
+    return companies[0]
+
+
+def match_fitting_applicant(company, applicants):
+    with open("sample_entry_applicants.txt", "w") as file:
+        for applicant in applicants:
+            file.write(str(applicant) + "\n")
+    return applicants[0]
