d35dd68f8603eb543d6353d828463bad98a48b0a
zedpaixd <planta636@gmail.com>
2024-11-09 13:37:53 +0200

    added CORS middleware

--- app/server/app.py
+++ app/server/app.py
@@ -3,8 +3,21 @@ from fastapi import FastAPI # type: ignore
 from server.routes.applicant import router as ApplicantRouter
 from server.routes.company import router as CompanyRouter
 from server.routes.matching import router as MatchingRouter
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+]
 
 app = FastAPI() # type: ignore
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows specific origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(ApplicantRouter, tags=["Applicant"], prefix="/applicant")
 app.include_router(CompanyRouter, tags=["Company"], prefix="/company")
